27d172758c904adfadca849bbd0ed8c69f628714
Soehtun <soehtuncu@gmail.com>
2020-07-31 13:59:54 +0630

    Add edit table

--- src/views/EditableCell.vue
+++ src/views/EditableCell.vue
@@ -1,100 +1,149 @@
 <template>
-  <div @click="onFieldClick" class="edit-cell">
-    <el-tooltip v-if="!editMode && !showInput"
-                :placement="toolTipPlacement"
-                :open-delay="toolTipDelay"
-                :content="toolTipContent">
-      <div tabindex="0" @keyup.enter="onFieldClick">
-        <slot name="content"></slot>
-      </div>
+  <div>
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      :select-options="{ enabled: true, selectOnCheckboxOnly: true }"
+      :search-options="{ enabled: true }"
+      :pagination-options="{
+        enabled: true,
+        mode: 'pages',
+        perPageDropdown: [5, 10, 15]
+      }">
 
-    </el-tooltip>
-    <component :is="editableComponent"
-               v-if="editMode || showInput"
-              ref="input"
-              @focus="onFieldClick"
-              @keyup.enter.native="onInputExit"
-              v-on="listeners"
-              v-bind="$attrs"
-              v-model="model">
-        <slot name="edit-component-slot"></slot>
-    </component>
+      <template slot="table-row" slot-scope="props">
+
+        <div v-if="props.column.field == 'btn'" style="width: 10px">
+          <el-button  v-if="props.row.id == editId" type="success" size="mini" @click="okRow(props.row)">Save</el-button>
+          <el-button v-else type="primary" size="mini" @click="editRow(props.row.id)">Edit</el-button>
+        </div>
+
+        <div v-else-if="props.column.html" v-html="props.row[props.column.field]">
+        
+        </div>
+
+        <div v-else>
+          <div v-if="props.row.id == editId">           
+            <el-date-picker v-if="props.column.custom=='date'" size="small" v-model="props.row[props.column.field]"></el-date-picker>
+            <el-select v-else-if="props.column.custom=='select'" size="small" v-model="props.row[props.column.field]" placeholder="Select">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+            <el-input v-else size="small" v-model="props.row[props.column.field]"/>
+          </div>
+
+          <div v-else>
+            <span>{{props.row[props.column.field]}}</span>
+          </div>
+        </div>
+        
+      </template>
+      
+    </vue-good-table>
+
   </div>
 </template>
+
 <script>
 export default {
-  name: "editable-cell",
-  inheritAttrs: false,
-  props: {
-    value: {
-      type: String,
-      default: ""
-    },
-    toolTipContent: {
-      type: String,
-      default: "Click to edit"
-    },
-    toolTipDelay: {
-      type: Number,
-      default: 500
-    },
-    toolTipPlacement: {
-      type: String,
-      default: "top-start"
-    },
-    showInput: {
-      type: Boolean,
-      default: false
-    },
-    editableComponent: {
-      type: String,
-      default: "el-input"
-    },
-    closeEvent: {
-      type: String,
-      default: "blur"
-    }
-  },
-  data() {
+  name: 'vueGood',
+  data(){
     return {
-      editMode: false
+      options: [{
+          value: 'Option1',
+          label: 'Option1'
+        }, {
+          value: 'Option2',
+          label: 'Option2'
+        }, {
+          value: 'Option3',
+          label: 'Option3'
+        }
+      ],
+      columns: [
+        {
+          label: 'action',
+          field: 'btn',
+          width: '50px'
+        },
+        {
+          label: 'Name',
+          field: 'name',
+          width: '100px'
+        },
+        {
+          label: 'Age',
+          field: 'age',
+          type: 'number',
+          width: '100px',
+          tdClass: this.tdClassFunc
+        },
+        {
+          label: 'HTML',
+          field: 'ht',
+          width: '200px',
+          html: true
+        },        
+        {
+          label: 'Date',
+          field: 'createdAt',
+          type: Date,
+          custom: 'date',
+          width: '250px'
+        },
+        {
+          label: 'Select',
+          field: 'score',
+          custom: 'select'
+        },
+      ],
+      rows: [
+        { id:1, ht:"<span style='color: green;'>HTML element</span>", name:"John", age: 20, createdAt: '', score: 'Option1' },
+        { id:2, name:"Jane", age: 24, createdAt: '2011-10-31', score: 'Option1' },
+        { id:3, name:"Susan", age: 16, createdAt: '2011-10-30', score: 'Option1' },
+        { id:4, name:"Chris", age: 55, createdAt: '2011-10-11', score: 'Option3' },
+        { id:5, name:"Dan", age: 40, createdAt: '2011-10-21', score: 'Option1' },
+        { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 'Option2' },
+      ],
+      edit: false,
+      old: {},
+      editId: ''
     };
   },
-  computed: {
-    model: {
-      get() {
-        return this.value;
-      },
-      set(val) {
-        this.$emit("input", val);
-      }
-    },
-    listeners() {
-      return {
-        [this.closeEvent]: this.onInputExit,
-        ...this.$listeners
-      };
-    }
-  },
   methods: {
-    onFieldClick() {
-      this.editMode = true;
-      this.$nextTick(() => {
-        let inputRef = this.$refs.input;
-        if (inputRef) {
-          inputRef.focus();
-        }
-      });
+    tdClassFunc(row) {
+      console.log('row', row)
+      // if (row.field > 50) {
+      //   return 'red-class';
+      // }
+      return 'green-class';
     },
-    onInputExit() {
-      this.editMode = false;
+    editRow(id){
+      this.editId = id;
+      console.log('helo',id)
+      this.edit = !this.edit;
+     
     },
-    onInputChange(val) {
-      this.$emit("input", val);
-    }
+    okRow(data) {
+      console.log('updatedata', data)
+      this.editId = ''
+    },
   }
 };
 </script>
-<style>
-
+<style scoped>
+.red-class {
+  color: red;
+}
+.green-class {
+  color: green;
+  font-size: 30px;
+}
+.custom{
+  width: 20px;
+}
 </style>
